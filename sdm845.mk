9d78130db9897011ad2f2f6ec81bf982891749f8
MarijnS95 <marijns95@gmail.com>
2019-12-03 19:25:06 +0100

    sdm845-common: Switch to display mapper/allocator 3.0.
    Add the new (oddly named) mapper and allocator v3.0 to PRODUCT_PACKAGES
    to get them built on supported devices/platforms. Other devices will
    still receive the v2.0 HALs.
    
    Quoting @kholk:
    Also, enable the new display render intents that these new HAL
    components require.
    
    Signed-off-by: MarijnS95 <marijns95@gmail.com>
    Change-Id: I8c56ea58ee9ee8b4f13e4e63ba3c71cd689a497a

--- sdm845.mk
+++ sdm845.mk
@@ -142,7 +142,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sdm845 \
@@ -156,7 +156,7 @@ PRODUCT_PACKAGES += \
     memtrack.sdm845 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
-    vendor.qti.hardware.display.allocator@1.0-service
+    vendor.qti.hardware.display.allocator-service
 
 # DRM
 PRODUCT_PACKAGES += \
