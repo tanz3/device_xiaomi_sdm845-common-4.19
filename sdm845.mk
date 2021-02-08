a1d93293ce3d9b4d50dc1dc937d6aa1e10e61b2b
Marijn Suijten <marijns95@gmail.com>
2021-02-08 01:05:51 +0100

    sdm845-common: Add (unused) qti mapper@2.0 interface libs for blobs
    The blobs link against this interface library because of a fallback
    mechanism when mapper@3.0 isn't made available (all our devices should
    always provide that, never 2.0). Consequently this results in a silent
    failure (not even a linker error appears) when trying to open the GPU
    through EGL:
    
        I AdrenoGLES-0: Driver Path                      : /odm/lib64/egl/libGLESv2_adreno.so
        W libEGL  : eglInitialize(0xb40000743497a2d0) failed (EGL_BAD_ALLOC)
        E libEGL  : call to OpenGL ES API with no current context (logged once per thread)
        F RenderEngine: eglQueryStringImplementationANDROID(EGL_VERSION) failed
    
    Adding this library - which is not explicitly linked/used by any other
    bits in the OSS part of the stack - resolves that error.
    
    Note that while the composer has a similar fallback mechanism to 2.0 it
    only uses (and links against) the AOSP interface
    (`android.hardware.graphics.mapper@2.0`), not the QTI one at
    `vendor.qti.hardware.display.mapper@2.0`.
    
    Note^2: All these hidl interfaces are defined as `system_ext`, which is
    the desired place for packages (in this case interface
    updates/extensions to the AOSP variant) may they ever land upstream.
    We have not configured such a partition currently (on super) but instead
    rely on it being in `/system/system_ext`. This is not compatible with
    GSIs; we just request the vendor variant to be put on `/vendor`
    (`.vendor` postfix on the package name).
    
    Signed-off-by: Marijn Suijten <marijns95@gmail.com>
    Change-Id: Ia6551128ffe23012880f5694179d78a7c35a1905

--- sdm845.mk
+++ sdm845.mk
@@ -154,7 +154,8 @@ PRODUCT_PACKAGES += \
     memtrack.sdm845 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
-    vendor.qti.hardware.display.allocator-service
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
