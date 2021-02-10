c93e6d4c088a03056ef696ebb0664cce8871f89a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:06:22 +0100

    sdm845-common: Build QTI Framework detect libs
    Change-Id: Ic4db96f8bd5a19f4805bd9c40544ed51d0352c61

--- sdm845.mk
+++ sdm845.mk
@@ -289,7 +289,10 @@ PRODUCT_PACKAGES += \
 
 # QTI
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
