1b72e8e9c9d2a0a234a154a06909a7edc4cef5c1
tanz3 <3222839916@qq.com>
2022-12-27 19:03:52 +0800

    sdm845-common: Switch to xiaomi common light hal
    Signed-off-by: tanz3 <3222839916@qq.com>

--- sdm845.mk
+++ sdm845.mk
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm845
+    android.hardware.light-service.xiaomi
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
