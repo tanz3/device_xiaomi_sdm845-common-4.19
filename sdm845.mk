30a355c57c5a5fcb80a3a30fe9c0631ce2ccadad
tanz3 <3222839916@qq.com>
2023-07-18 13:24:40 -0400

    sdm845-common: Update neural blobs from munch
    Signed-off-by: tanz3 <3222839916@qq.com>

--- sdm845.mk
+++ sdm845.mk
@@ -278,7 +278,7 @@ PRODUCT_PACKAGES += \
 
 # Neural Network
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.2.vendor
+    android.hardware.neuralnetworks@1.3.vendor
 
 # Perfd (dummy)
 PRODUCT_PACKAGES += \
